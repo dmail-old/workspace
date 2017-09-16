980841e07b611e3698ef93832cb732433a7c070b
Damien Maillard <dmaillard06@gmail.com>
2017-09-16 14:18:41 +0200

    update sample repositories

--- scripts/gitClone.js
+++ scripts/gitClone.js
@@ -2,7 +2,12 @@ const { execCommand, getPackagesFolder } = require("./util/index.js")
 
 const command = "git.exe"
 const args = ["clone"]
-const repositories = ["dmail/shared-config"]
+const repositories = [
+	"dmail/sample-common-dependency",
+	"dmail/sample-main",
+	"dmail/sample-dependency",
+	"dmail/shared-config"
+]
 
 repositories
 	.reduce((previous, repository) => {
