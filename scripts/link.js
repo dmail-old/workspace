bff94991d37fa94298c375f1986692032b5355a7
Damien Maillard <dmaillard06@gmail.com>
2017-09-27 19:38:30 +0200

    fix link on windows

--- scripts/link.js
+++ scripts/link.js
@@ -1,11 +1,13 @@
 // a chokidar to auto rerun this when any directory gets added/removed from packages/* ??
 
+const path = require("path")
 const { createCommand, exposeModuleCommand } = require("@dmail/command")
 const { getPackagesFolder } = require("./util/index.js")
 
 const createSymlinkCommand = () =>
 	createCommand({
-		name: "../node_modules/.bin/symlink",
+		name: path.resolve(__dirname, "../node_modules/.bin/symlink"),
+		windowsExtension: "cmd",
 		args: [".", "--execute"],
 		cwd: getPackagesFolder()
 	})
